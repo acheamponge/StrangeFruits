2f4fcd5e7443d83effacdec89f0fc2477cfd6851
acheamponge <achampion.emma@gmail.com>
2020-01-24 15:02:24 -0500

    included data analytics of the dataset

--- streamlitapp/src/pages/analytics.py
+++ streamlitapp/src/pages/analytics.py
@@ -14,16 +14,31 @@ def write():
             }
             
     pick = st.selectbox("Select Dataset: ", list(keys))
-
+    
+    
+    analysis = {
+    'count'
+            }
+            
     
     df = pd.read_csv(pick, encoding='utf8')
+    
+    
+
+    
+    
+    df.columns = ['State','Year','Month','Day','Victim','County','Race','Sex','Mob','Offense','Note','2nd Name','3rd Name','Comments','Source']
     st.dataframe(df)
     
+    ana = st.selectbox("Select Aggregation: ", list(analysis))
+    
+    if ana=='count':
+        st.info("Total Number of Lynchings: " + str(df.shape[0]))
     
     group = {
     'State',
     'Year',
-    'Mo',
+    'Month',
     'Race',
     'County',
     'Offense'    
@@ -31,5 +46,8 @@ def write():
     pick_grp = st.selectbox("Groupby: ", list(group))
     
     df3 = df.groupby(pick_grp).count()
+    
+    df3 = df3[['Victim']]
+    df3.columns = ['Count']
     
     st.dataframe(df3)
